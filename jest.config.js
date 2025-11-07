b3b2593f5864d6da2a868b88f0ff2363c7682035
Gonzalo-Rod <gonzalo.rodriguez.g@utec.edu.pe>
2025-11-07 15:04:01 -0500

    new pipline

--- jest.config.js
+++ jest.config.js
@@ -4,6 +4,7 @@ module.exports = {
   transformIgnorePatterns: [
     "node_modules/(?!((jest-)?@?react-native|@react-native-async-storage|@react-navigation|@gorhom|expo(nent)?|@expo(nent)?|expo-modules-core|@expo-google-fonts|react-clone-referenced-element|react-native|react-native-.*|@react-native-.*)/)",
   ],
+  testPathIgnorePatterns: ["<rootDir>/node_modules/", "<rootDir>/backend/"],
   moduleNameMapper: {
     "\\.svg$": "<rootDir>/__mocks__/svgMock.js",
     "\\.(jpg|jpeg|png|gif|webp|ico|bmp)$": "<rootDir>/__mocks__/fileMock.js",
